f87b1e68d648daac1cba2dbb54a774b266d2ae98
Ingram Braun <CarlOrff@users.noreply.github.com>
2022-12-31 09:34:45 +0100

    blacklist Pocket

--- mirror.pl
+++ mirror.pl
@@ -11,7 +11,7 @@ use LWP::RobotUA;
 use WWW::RobotRules;
 use URI;
 
-my $start_url = qw{ https://www.meinschlossringenberg.de/ };
+my $start_url = qw{ https://agentur-notabene.de/hans-peter-hallwachs-vita/ };
 my $pattern = '';
 my $host = URI->new(URI->new($start_url)->canonical)->host;
 
