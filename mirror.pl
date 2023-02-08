646a769ce59e722a79269983666c8ad0a371d90f
Ingram Braun <CarlOrff@users.noreply.github.com>
2023-02-08 10:31:31 +0100

    Firefox timeout protection

--- mirror.pl
+++ mirror.pl
@@ -11,7 +11,7 @@ use LWP::RobotUA;
 use WWW::RobotRules;
 use URI;
 
-my $start_url = qw{ https://rommyarndt.de/ };
+my $start_url = qw{ http://www.systemerror.info/ };
 my $pattern = '';
 my $host = URI->new(URI->new($start_url)->canonical)->host;
 
