cb5f767876d6586fbae9ee2e39e4751476c8ddc3
Ingram Braun <CarlOrff@users.noreply.github.com>
2023-06-01 05:57:36 +0200

    blacklist extended (Tumblr)

--- mirror.pl
+++ mirror.pl
@@ -11,7 +11,7 @@ use LWP::RobotUA;
 use WWW::RobotRules;
 use URI;
 
-my $start_url = qw{ http://www.keltentruppe.de/ };
+my $start_url = qw{ http://www.management-goldschmidt.de/p/en/actor/peter-simonischek/ };
 my $pattern = '';
 my $host = URI->new(URI->new($start_url)->canonical)->host;
 
