6a0bbb71cbfe633af714e65379fd53bc0d895b5f
Ingram Braun <CarlOrff@users.noreply.github.com>
2023-07-13 13:02:43 +0200

    blacklist Parler

--- mirror.pl
+++ mirror.pl
@@ -11,7 +11,7 @@ use LWP::RobotUA;
 use WWW::RobotRules;
 use URI;
 
-my $start_url = qw{ https://die-agenten.de/profile/melika-foroutan/ };
+my $start_url = qw{ http://www.mueller-kademann.de/ };
 my $pattern = '';
 my $host = URI->new(URI->new($start_url)->canonical)->host;
 
