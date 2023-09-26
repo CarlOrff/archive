add7eeb6040933234d1c2ab6af2351137bf44e0a
Ingram Braun <CarlOrff@users.noreply.github.com>
2023-09-26 16:36:54 +0200

    blacklist (Add This)

--- mirror.pl
+++ mirror.pl
@@ -11,7 +11,7 @@ use LWP::RobotUA;
 use WWW::RobotRules;
 use URI;
 
-my $start_url = qw{ https://ammann-horak.agency/index.php/de/kuenstler-de/artist-gould-stephen-de };
+my $start_url = qw{ https://archaeologyinside.wordpress.com/ };
 my $pattern = '';
 my $host = URI->new(URI->new($start_url)->canonical)->host;
 
