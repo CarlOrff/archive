8b1c0939243bcbfe634afd22374ad81c9972f50a
Ingram Braun <CarlOrff@users.noreply.github.com>
2024-10-03 21:52:56 +0200

    blacklist (WP nextdoor)

--- mirror.pl
+++ mirror.pl
@@ -11,7 +11,7 @@ use LWP::RobotUA;
 use WWW::RobotRules;
 use URI;
 
-my $start_url = qw{ http://www.knastblattaxel.com/ };
+my $start_url = qw{ https://stfrancisretreathouse.org };
 
 my $pattern = '';
 my $host = URI->new(URI->new($start_url)->canonical)->host;
