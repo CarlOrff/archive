c554d43115ea5b13663c6817e5a789e0ffe968e6
Ingram Braun <CarlOrff@users.noreply.github.com>
2025-05-18 00:55:03 +0200

    improved campaign detection

--- mirror.pl
+++ mirror.pl
@@ -11,7 +11,7 @@ use LWP::RobotUA;
 use WWW::RobotRules;
 use URI;
 
-my $start_url = qw{ https://www.gott-wissen.de/forum/ };
+my $start_url = qw{ https://komascript.de/ };
 
 my $pattern = '';
 my $host = URI->new(URI->new($start_url)->canonical)->host;
